aaa66f7931460db759d0e6a3a580e467d4dade27
laptop <laptop@laptops-MacBook-Pro.local>
2023-04-25 16:03:02 -0400

    Documentation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo
-echo 'Warning: Please ensure that you are running the latest firmware'
+echo 'Warning: Please ensure that you are running the latest firmware!'
+echo 'Device's side-switch should be in the down position (away from recessed dot).'
 echo
 read -p "Enter the IP address: " ip_address
 ssh root@$ip_address << 'ENDSSH'
@@ -11,4 +12,7 @@ opkg update
 opkg install blue-merle.ipk
 reboot
 ENDSSH
-echo 'Device will now reboot, please ensure that the switch is in the down position (away from recessed dot)'
+echo 'Device will now reboot!'
+echo
+echo 'When device is fully booted, flip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.'
+echo
