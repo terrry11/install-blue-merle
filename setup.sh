484f559cd55c46d04aa34c1cfeafd29714265978
laptop <laptop@laptops-MBP.lan>
2023-04-29 17:30:31 -0400

    Fully operational functions

--- setup.sh
+++ setup.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
-# Pre-install messages
+# Pre-install messages.
 pre_install() {
     printf "\nWarning: Please ensure that you are running the latest firmware!\n\n"
     printf "Device's side-switch should be in the down position (away from recessed dot).\n\n"
 }
 
-# Query IP from user and GH API for latest download URL
+# Query IP from user and GH API for latest download URL.
 init_vars() {
     read -p "Enter IP address: " ip_address
     local api_url='https://api.github.com/repos/srlabs/blue-merle/releases/latest'
-    down_url=$(curl -sL $api_url | grep browser_download | awk -F '"' '{print $4}')
+    down_url=$(curl -sL $api_url | grep browser_download | awk -F \" '{print $4}')
 }
 
-# Post-install messages
+# Post-install messages.
 post_install() {
     printf '\n\nInstall complete, device will now reboot!\n'
-    printf 'After device boots:\nFlip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.\n\n'
+    printf '\nAfter device boots:\nFlip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.\n\n'
 }
 
-# Initialization
+# Test.
+test() {
+    printf '\nIP:'
+    echo $ip_address
+    printf '\nGH: '
+    echo $down_url
+}
+
+remote_install() {
+    ssh root@$ip_address -oHostKeyAlgorithms=+ssh-rsa << ENDSSH
+    curl -L $down_url -o /tmp/blue-merle.ipk
+    opkg update
+    opkg install /tmp/blue-merle.ipk
+    reboot
+ENDSSH
+}
+
+# Main.
 pre_install
 init_vars
-
-# Begin SSH Connection
-ssh root@$ip_address << 'ENDSSH'
-curl -L $down_url -o /tmp/blue-merle.ipk
-opkg update
-opkg install /tmp/blue-merle.ipk
-reboot
-ENDSSH
-
-# Finish
+test
+remote_install
 post_install
