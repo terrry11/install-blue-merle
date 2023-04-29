d9f4f3fb4554f65b7146da8a6d020592033f95b3
laptop <laptop@laptops-MacBook-Pro.local>
2023-04-29 13:45:55 -0400

    Script now descovers latest version, no hardcoded version numbers

--- setup.sh
+++ setup.sh
@@ -5,7 +5,10 @@ printf "\nWarning: Please ensure that you are running the latest firmware!\n\n"
 printf "Device's side-switch should be in the down position (away from recessed dot).\n\n"
 
 # Initialize variables
-latest='https://github.com/srlabs/blue-merle/releases/download/v1.0/blue-merle_1.0.0-1_mips_24kc.ipk'
+get_latest() {
+    local api_url='https://api.github.com/repos/srlabs/blue-merle/releases/latest'
+    latest=$(curl -sL $api_url | grep browser_download | awk -F '"' '{print $4}')
+}
 read -p "Enter IP address: " ip_address
 
 # Begin SSH Connection
@@ -17,6 +20,6 @@ opkg install blue-merle.ipk
 reboot
 ENDSSH
 
-# Post-install user considerations
+# Post-install considerations
 printf '\n\nInstall complete, device will now reboot!\n'
 printf 'After device boots:\nFlip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.\n\n'
