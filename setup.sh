4edac8c5b74ce43bfba6d0388eafbce53598a583
laptop <laptop@laptops-MacBook-Pro.local>
2023-04-29 14:05:18 -0400

    Refactored

--- setup.sh
+++ setup.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-# Warn user
-printf "\nWarning: Please ensure that you are running the latest firmware!\n\n"
-printf "Device's side-switch should be in the down position (away from recessed dot).\n\n"
+# Pre-install messages
+pre_install() {
+    printf "\nWarning: Please ensure that you are running the latest firmware!\n\n"
+    printf "Device's side-switch should be in the down position (away from recessed dot).\n\n"
+}
 
-# Initialize variables
+# Get latest download URL
 get_latest() {
     local api_url='https://api.github.com/repos/srlabs/blue-merle/releases/latest'
-    latest=$(curl -sL $api_url | grep browser_download | awk -F '"' '{print $4}')
+    down_url=$(curl -sL $api_url | grep browser_download | awk -F '"' '{print $4}')
 }
+
+# Post-install messages
+post_install() {
+    printf '\n\nInstall complete, device will now reboot!\n'
+    printf 'After device boots:\nFlip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.\n\n'
+}
+
+# Initialization
+pre_install
 read -p "Enter IP address: " ip_address
+get_latest
 
 # Begin SSH Connection
 ssh root@$ip_address << 'ENDSSH'
 cd /tmp
-wget $latest -O blue-merle.ipk
+wget $down_url -O blue-merle.ipk
 opkg update
 opkg install blue-merle.ipk
 reboot
 ENDSSH
 
-# Post-install considerations
-printf '\n\nInstall complete, device will now reboot!\n'
-printf 'After device boots:\nFlip side-switch to the up position (towards recessed dot) and follow on-device MCU prompts.\n\n'
+# Finish
+post_install
