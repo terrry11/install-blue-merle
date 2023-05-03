ed90578612decaaf0a1bc6d04edc5d261bfc7c8f
oldstreetloft <consulting@oldstreetloft.io>
2023-05-03 15:15:09 -0400

    Check for down_url

--- setup.sh
+++ setup.sh
@@ -32,13 +32,13 @@ test_conn() {
         printf "\nProvided IP Address: $ip_addr\n\nDevice is responding.\n\n"
     else
         printf "\nERROR: No route to device!\nAre you behind a VPN or connected to the wrong network?\n"
-        printf "Please ensure connectivity to device and try again.\n\n" ; exit 0
+        printf "Please ensure connectivity to device and try again.\n\n" ; exit 1
     fi
     if ping -c 1 1.1.1.1 &> /dev/null ; then
         printf "You are connected to the internet.\n\n"
     else
         printf "\nERROR: You are NOT connected to the internet.\n\n"
-        printf "Please ensure internet connectivity and try again.\n\n" ; exit 0
+        printf "Please ensure internet connectivity and try again.\n\n" ; exit 1
     fi
 }
 
@@ -47,7 +47,12 @@ parse_github() {
     local auth_repo='srlabs/blue-merle'
     local api_url="https://api.github.com/repos/$auth_repo/releases/latest"
     down_url=$(curl -sL $api_url | grep browser_download | awk -F \" '{print $4}')
-    printf "Latest GH download URL: \n$down_url\n\n"
+    if [ -z "$down_url" ]; then
+        printf "ERROR: Unable to retrieve latest download URL from GitHub API.\n"
+        exit 1
+    else
+        printf "Latest GH download URL: \n$down_url\n\n"
+    fi
 }
 
 # Detect the OS of the host, install dependencies.
@@ -68,7 +73,7 @@ ssh root@$ip_addr -oHostKeyAlgorithms=+ssh-rsa << ENDSSH
 
 # Check to see if blue-merle is already installed.
 echo ; if opkg list | grep blue-merle ; then
-    printf "\nPackage is already installed!\n\nExiting...\n" ; exit 0
+    printf "\nPackage is already installed!\n\nExiting...\n" ; exit 1
 else
     printf "\nStarting install.\n\nDevice will reboot upon completion...\n\n" ; sleep 1
 fi
@@ -82,7 +87,7 @@ if curl -L $down_url -o /tmp/blue-merle.ipk ; then
     printf "Follow on-device MCU prompts.\n" ; reboot
 else
     printf "\nERROR: Device is NOT connected to the internet.\n"
-    printf "Please ensure internet connectivity and try again.\n\n" ; exit 0
+    printf "Please ensure internet connectivity and try again.\n\n" ; exit 1
 fi
 ENDSSH
 }
