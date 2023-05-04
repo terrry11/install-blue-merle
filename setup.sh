51b8f6d8c3483b55e63d1ffa31986efd6c00ad24
oldstreetloft <consulting@oldstreetloft.io>
2023-05-04 09:05:41 -0400

    Check github.com instead to check dns resolution

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ main() {
     pre_install             # Pre-install message.
     parse_args $1           # Get data from user.
     test_conn               # Exit if no connection.
-    parse_github            # Query GH for latest download URL.
+    parse_github            # Query GH for download URL.
     detect_os               # Install dependencies.
     ssh_install             # Install script.
 }
@@ -38,15 +38,15 @@ get_ip() {
     fi
 }
 
-# Check to see if device and 1.1.1.1 are responding.
+# Check to see if device and Github are responding.
 test_conn() {
-    if ping -c 1 $ip_addr &> /dev/null ; then
+    if nc -z -w1 $ip_addr 22 &> /dev/null ; then
         printf "\nProvided IP Address: $ip_addr\n\nDevice is responding.\n\n"
     else
         printf "\nERROR: No route to device!\nAre you behind a VPN or connected to the wrong network?\n"
         printf "Please ensure connectivity to device and try again.\n\n" ; exit 1
     fi
-    if ping -c 1 1.1.1.1 &> /dev/null ; then
+    if ping -c 1 github.com &> /dev/null ; then
         printf "You are connected to the internet.\n\n"
     else
         printf "\nERROR: You are NOT connected to the internet.\n\n"
